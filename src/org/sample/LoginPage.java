6fa2f166f29b9fd1dd1b0e0c8cfe86dfe2b54d42
panchal-sunil <panchalsunil133@gmail.com>
2022-06-25 19:47:15 +0530

    commited

---
+++ src/org/sample/LoginPage.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mathaiah\\eclipse-workspace\\FaceBook\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		driver.findElement(By.id("email")).sendKeys("sunil");
+		driver.findElement(By.id("pass")).sendKeys("123456");
+		
+		driver.findElement(By.name("login")).click();
+		
+	}
+}
